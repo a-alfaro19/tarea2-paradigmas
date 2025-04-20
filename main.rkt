4299ea90db6d758010cb3cbd148a3ec25be05913
melivasr <anavasquez@estudiantec.cr>
2025-04-20 09:53:54 -0600

    Documentacion interna

--- main.rkt
+++ main.rkt
@@ -1,16 +1,32 @@
 #lang racket
+
 (require "cubo.rkt")
 (require "movimientos.rkt")
 
 (provide RS)
 
-;; RS Número Lista Lista -> Lista
-;; Ejecuta una serie de movimientos sobre un cubo n x n
-(define (RS n cubo movimientos)
-  (aplicar-movimientos n cubo movimientos))
+;; -----------------------------------------------------------------------------
+;; RS: Número Lista Lista -> Lista
+;; Función principal del simulador RubikSimulator.
+;; Si la lista de cubo está vacía, se genera automáticamente un cubo nxn.
+;; Luego se aplican los movimientos indicados en orden.
+;;
+;; Parámetros:
+;; - n: Tamaño del cubo (entero entre 2 y 6).
+;; - cubo: Lista de 6 caras (matrices) o lista vacía.
+;; - movs: Lista de movimientos como símbolos (e.g., 'F1D, 'C2A).
+;;
+;; Retorna:
+;; - El cubo resultante tras aplicar todos los movimientos.
+;; -----------------------------------------------------------------------------
+(define (RS n cubo movs)
+  (cond
+    [(null? movs) cubo]
+    [else
+     (RS n (aplicar-movimiento n cubo (car movs)) (cdr movs))]))
 
-;; EJEMPLO DE USO:
-;; (define cubo (crear-cubo 2))
-;; (RS 2 cubo '(F1D C2A F2I C1B))
 
+;(define cubo-ejemplo (crear-cubo 2))
+
+;(RS 2 cubo-ejemplo '(F1I F2D))
 
