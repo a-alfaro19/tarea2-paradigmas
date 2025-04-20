ec8059d2941d4ee7e49f0592973f216263412465
melivasr <anavasquez@estudiantec.cr>
2025-04-20 08:15:32 -0600

    Movimientos funcionales con casos extremos

--- main.rkt
+++ main.rkt
@@ -1,23 +1,16 @@
-#lang scheme
+#lang racket
 (require "cubo.rkt")
 (require "movimientos.rkt")
 
-;; -----------------------------------------------------------------------------
-;; aplicar-movimientos Número Lista Lista -> Lista
-;;
-;; Aplica una secuencia de movimientos al cubo, uno por uno.
-;;
-;; Parámetros:
-;; - n: tamaño del cubo (n x n).
-;; - cubo: lista de 6 caras (el estado actual del cubo).
-;; - movs: lista de movimientos (ej: '(F1D C2B)).
-;;
-;; Retorna:
-;; - Cubo con todos los movimientos aplicados.
-;; -----------------------------------------------------------------------------
-(define (aplicar-movimientos n cubo movs)
-  (cond
-    [(null? movs)
-     cubo]
-    [else
-     (aplicar-movimientos n (aplicar-movimiento n cubo (car movs)) (cdr movs))]))
+(provide RS)
+
+;; RS Número Lista Lista -> Lista
+;; Ejecuta una serie de movimientos sobre un cubo n x n
+(define (RS n cubo movimientos)
+  (aplicar-movimientos n cubo movimientos))
+
+;; EJEMPLO DE USO:
+;; (define cubo (crear-cubo 2))
+;; (RS 2 cubo '(F1D C2A F2I C1B))
+
+
--- movimientos.rkt
+++ movimientos.rkt
@@ -1,114 +1,222 @@
 #lang racket
 
-(provide rotar-columna-en-cubo
+(provide aplicar-movimiento
          rotar-fila-en-cubo
-         columna-a-lista
-         reemplazar-columna)
+         rotar-columna-en-cubo
+         rotar-cara-horaria
+         rotar-cara-antihoraria
+         es-extremo-fila?
+         es-extremo-columna?
+         extraer-fila
+         reemplazar-fila
+         extraer-columna
+         reemplazar-columna
+         numero)
 
-;; =============================================================================
-;; FUNCIONES AUXILIARES
-;; =============================================================================
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utilitarios Básicos
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (columna-a-lista cara i)
+(define (numero c) (- (char->integer c) 48))
+
+(define (invertir-lista lst)
+  (invertir-aux lst '()))
+
+(define (invertir-aux lst acc)
+  (cond
+    [(null? lst) acc]
+    [else (invertir-aux (cdr lst) (cons (car lst) acc))]))
+
+(define (obtener-elemento fila indice)
+  (cond
+    [(= indice 0) (car fila)]
+    [else (obtener-elemento (cdr fila) (- indice 1))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Rotaciones de caras (matriz) 90° horario y antihorario
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (rotar-cara-horaria cara)
+  (rotar-cara-horaria-aux cara 0 (length cara)))
+
+(define (rotar-cara-horaria-aux cara col total)
+  (cond
+    [(= col total) '()]
+    [else
+     (cons (obtener-columna-invertida cara col)
+           (rotar-cara-horaria-aux cara (+ col 1) total))]))
+
+(define (rotar-cara-antihoraria cara)
+  (rotar-cara-antihoraria-aux cara (- (length cara) 1) -1))
+
+(define (rotar-cara-antihoraria-aux cara col final)
+  (cond
+    [(= col final) '()]
+    [else
+     (cons (obtener-columna cara col)
+           (rotar-cara-antihoraria-aux cara (- col 1) final))]))
+
+(define (obtener-columna cara col)
   (cond
     [(null? cara) '()]
     [else
-     (cons (elemento-en (car cara) (- i 1)) (columna-a-lista (cdr cara) i))]))
+     (cons (obtener-elemento (car cara) col)
+           (obtener-columna (cdr cara) col))]))
 
-(define (reemplazar-columna cara nueva-columna i)
-  (reemplazar-columna-aux cara nueva-columna i '()))
+(define (obtener-columna-invertida cara col)
+  (invertir-lista (obtener-columna cara col)))
 
-(define (reemplazar-columna-aux cara nueva i acc)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Detección de extremos
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (es-extremo-fila? fila n)
   (cond
-    [(null? cara) acc]
+    [(or (= fila 1) (= fila n)) #t]
+    [else #f]))
+
+(define (es-extremo-columna? columna n)
+  (cond
+    [(or (= columna 1) (= columna n)) #t]
+    [else #f]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Manipulación de filas y columnas
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (extraer-fila matriz i)
+  (cond
+    [(= i 1) (car matriz)]
+    [else (extraer-fila (cdr matriz) (- i 1))]))
+
+(define (reemplazar-fila matriz i nueva-fila)
+  (cond
+    [(= i 1) (cons nueva-fila (cdr matriz))]
+    [else (cons (car matriz)
+                (reemplazar-fila (cdr matriz) (- i 1) nueva-fila))]))
+
+(define (extraer-columna matriz j)
+  (cond
+    [(null? matriz) '()]
+    [else (cons (obtener-elemento (car matriz) (- j 1))
+                (extraer-columna (cdr matriz) j))]))
+
+(define (reemplazar-columna matriz j nueva-columna)
+  (cond
+    [(null? matriz) '()]
     [else
-     (reemplazar-columna-aux (cdr cara)
-                              (cdr nueva)
-                              i
-                              (append acc
-                                      (list (reemplazar-en (car cara) (- i 1) (car nueva)))))]))
+     (cons (reemplazar-en-fila (car matriz) j (car nueva-columna))
+           (reemplazar-columna (cdr matriz) j (cdr nueva-columna)))]))
 
-(define (elemento-en lista i)
+(define (reemplazar-en-fila fila j nuevo)
   (cond
-    [(zero? i) (car lista)]
-    [else (elemento-en (cdr lista) (- i 1))]))
+    [(= j 1) (cons nuevo (cdr fila))]
+    [else (cons (car fila)
+                (reemplazar-en-fila (cdr fila) (- j 1) nuevo))]))
 
-(define (reemplazar-en lista i nuevo)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Movimiento principal con casos extremos - funcional puro
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (reemplazar-en-lista lista pos nuevo)
   (cond
-    [(null? lista) '()]
-    [(zero? i) (cons nuevo (cdr lista))]
-    [else (cons (car lista) (reemplazar-en (cdr lista) (- i 1) nuevo))]))
+    [(= pos 0) (cons nuevo (cdr lista))]
+    [else (cons (car lista) (reemplazar-en-lista (cdr lista) (- pos 1) nuevo))]))
 
-(define (fila-a-lista cara fila-num)
-  (elemento-en cara (- fila-num 1)))
+(define (rotar-fila-en-cubo n cubo fila direccion)
+  (cond
+    [(es-extremo-fila? fila n)
+     (define nuevo-cubo (rotar-fila-extremo cubo fila direccion))
+     (cond
+       [(and (= fila 1) (equal? direccion 'derecha))
+        (reemplazar-en-lista nuevo-cubo 0 (rotar-cara-antihoraria (list-ref nuevo-cubo 0)))]
+       [(and (= fila 1) (equal? direccion 'izquierda))
+        (reemplazar-en-lista nuevo-cubo 0 (rotar-cara-horaria (list-ref nuevo-cubo 0)))]
+       [(and (= fila n) (equal? direccion 'derecha))
+        (reemplazar-en-lista nuevo-cubo 5 (rotar-cara-horaria (list-ref nuevo-cubo 5)))]
+       [(and (= fila n) (equal? direccion 'izquierda))
+        (reemplazar-en-lista nuevo-cubo 5 (rotar-cara-antihoraria (list-ref nuevo-cubo 5)))]
+       [else nuevo-cubo])]
+    [else (rotar-fila-extremo cubo fila direccion)]))
 
-(define (reemplazar-fila cara nueva-fila fila-num)
-  (reemplazar-en cara (- fila-num 1) nueva-fila))
+(define (rotar-fila-extremo cubo fila direccion)
+  (define izq (extraer-fila (list-ref cubo 4) fila))
+  (define fr (extraer-fila (list-ref cubo 1) fila))
+  (define der (extraer-fila (list-ref cubo 2) fila))
+  (define post (extraer-fila (list-ref cubo 3) fila))
+  (cond
+    [(equal? direccion 'derecha)
+     (reemplazar-en-lista
+      (reemplazar-en-lista
+       (reemplazar-en-lista
+        (reemplazar-en-lista cubo 4 (reemplazar-fila (list-ref cubo 4) fila post))
+        1 (reemplazar-fila (list-ref cubo 1) fila izq))
+       2 (reemplazar-fila (list-ref cubo 2) fila fr))
+      3 (reemplazar-fila (list-ref cubo 3) fila der))]
 
-;; =============================================================================
-;; ROTACIÓN DE COLUMNAS (existente)
-;; =============================================================================
+    [(equal? direccion 'izquierda)
+     (reemplazar-en-lista
+      (reemplazar-en-lista
+       (reemplazar-en-lista
+        (reemplazar-en-lista cubo 4 (reemplazar-fila (list-ref cubo 4) fila fr))
+        1 (reemplazar-fila (list-ref cubo 1) fila der))
+       2 (reemplazar-fila (list-ref cubo 2) fila post))
+      3 (reemplazar-fila (list-ref cubo 3) fila izq))]))
 
-(define (rotar-columna-en-cubo n cubo i direccion)
-  (define cara-sup (elemento-en cubo 0))
-  (define cara-front (elemento-en cubo 1))
-  (define cara-post (elemento-en cubo 3))
-  (define cara-inf (elemento-en cubo 5))
 
-  (define col-sup (columna-a-lista cara-sup i))
-  (define col-front (columna-a-lista cara-front i))
-  (define col-post (columna-a-lista cara-post i))
-  (define col-inf (columna-a-lista cara-inf i))
+(define (rotar-columna-en-cubo n cubo columna direccion)
+  (define sup (extraer-columna (list-ref cubo 0) columna))
+  (define fr (extraer-columna (list-ref cubo 1) columna))
+  (define inf (extraer-columna (list-ref cubo 5) columna))
+  (define post (extraer-columna (list-ref cubo 3) (- n (- columna 1)))) ; invertir columna al entrar/salir
+  (define izq? (= columna 1))
+  (define der? (= columna n))
+  (cond
+    [(equal? direccion 'abajo)
+     (define nuevo-sup (invertir-lista post))
+     (define nuevo-fr sup)
+     (define nuevo-inf fr)
+     (define nuevo-post (invertir-lista inf))
+     (define cubo1 (reemplazar-en-lista cubo 0 (reemplazar-columna (list-ref cubo 0) columna nuevo-sup)))
+     (define cubo2 (reemplazar-en-lista cubo1 1 (reemplazar-columna (list-ref cubo1 1) columna nuevo-fr)))
+     (define cubo3 (reemplazar-en-lista cubo2 5 (reemplazar-columna (list-ref cubo2 5) columna nuevo-inf)))
+     (define nuevo-cubo (reemplazar-en-lista cubo3 3 (reemplazar-columna (list-ref cubo3 3) (- n (- columna 1)) nuevo-post)))
+     (cond
+       [izq? (reemplazar-en-lista nuevo-cubo 4 (rotar-cara-horaria (list-ref nuevo-cubo 4)))]
+       [der? (reemplazar-en-lista nuevo-cubo 2 (rotar-cara-antihoraria (list-ref nuevo-cubo 2)))]
+       [else nuevo-cubo])]
 
-  (define col-sup-nuevo (if (equal? direccion 'abajo) col-post col-front))
-  (define col-front-nuevo (if (equal? direccion 'abajo) col-sup col-inf))
-  (define col-inf-nuevo (if (equal? direccion 'abajo) col-front col-post))
-  (define col-post-nuevo (if (equal? direccion 'abajo) col-inf col-sup))
+    [(equal? direccion 'arriba)
+     (define nuevo-sup fr)
+     (define nuevo-fr inf)
+     (define nuevo-inf (invertir-lista post))
+     (define nuevo-post (invertir-lista sup))
+     (define cubo1 (reemplazar-en-lista cubo 0 (reemplazar-columna (list-ref cubo 0) columna nuevo-sup)))
+     (define cubo2 (reemplazar-en-lista cubo1 1 (reemplazar-columna (list-ref cubo1 1) columna nuevo-fr)))
+     (define cubo3 (reemplazar-en-lista cubo2 5 (reemplazar-columna (list-ref cubo2 5) columna nuevo-inf)))
+     (define nuevo-cubo (reemplazar-en-lista cubo3 3 (reemplazar-columna (list-ref cubo3 3) (- n (- columna 1)) nuevo-post)))
+     (cond
+       [izq? (reemplazar-en-lista nuevo-cubo 4 (rotar-cara-antihoraria (list-ref nuevo-cubo 4)))]
+       [der? (reemplazar-en-lista nuevo-cubo 2 (rotar-cara-horaria (list-ref nuevo-cubo 2)))]
+       [else nuevo-cubo])]))
 
-  (define cara-sup-nueva (reemplazar-columna cara-sup col-sup-nuevo i))
-  (define cara-front-nueva (reemplazar-columna cara-front col-front-nuevo i))
-  (define cara-inf-nueva (reemplazar-columna cara-inf col-inf-nuevo i))
-  (define cara-post-nueva (reemplazar-columna cara-post col-post-nuevo i))
 
-  (reemplazar-en
-   (reemplazar-en
-    (reemplazar-en
-     (reemplazar-en cubo 0 cara-sup-nueva)
-     1 cara-front-nueva)
-    3 cara-post-nueva)
-   5 cara-inf-nueva))
 
-;; =============================================================================
-;; ROTACIÓN DE FILAS (nuevo)
-;; =============================================================================
+(define (aplicar-movimiento n cubo mov)
+  (cond
+    [(and (equal? (string-ref (symbol->string mov) 0) #\F)
+          (equal? (string-ref (symbol->string mov) 2) #\D))
+     (rotar-fila-en-cubo n cubo (numero (string-ref (symbol->string mov) 1)) 'derecha)]
 
-(define (rotar-fila-en-cubo n cubo fila-num direccion)
-  (define cara-front (elemento-en cubo 1))
-  (define cara-der (elemento-en cubo 2))
-  (define cara-post (elemento-en cubo 3))
-  (define cara-izq (elemento-en cubo 4))
+    [(and (equal? (string-ref (symbol->string mov) 0) #\F)
+          (equal? (string-ref (symbol->string mov) 2) #\I))
+     (rotar-fila-en-cubo n cubo (numero (string-ref (symbol->string mov) 1)) 'izquierda)]
 
-  (define fila-front (fila-a-lista cara-front fila-num))
-  (define fila-der (fila-a-lista cara-der fila-num))
-  (define fila-post (fila-a-lista cara-post fila-num))
-  (define fila-izq (fila-a-lista cara-izq fila-num))
+    [(and (equal? (string-ref (symbol->string mov) 0) #\C)
+          (equal? (string-ref (symbol->string mov) 2) #\A))
+     (rotar-columna-en-cubo n cubo (numero (string-ref (symbol->string mov) 1)) 'arriba)]
 
-  (define nueva-front (if (equal? direccion 'derecha) fila-izq fila-der))
-  (define nueva-der (if (equal? direccion 'derecha) fila-front fila-post))
-  (define nueva-post (if (equal? direccion 'derecha) fila-der fila-izq))
-  (define nueva-izq (if (equal? direccion 'derecha) fila-post fila-front))
-
-  (define cara-front-nuevo (reemplazar-fila cara-front nueva-front fila-num))
-  (define cara-der-nuevo (reemplazar-fila cara-der nueva-der fila-num))
-  (define cara-post-nuevo (reemplazar-fila cara-post nueva-post fila-num))
-  (define cara-izq-nuevo (reemplazar-fila cara-izq nueva-izq fila-num))
-
-  (reemplazar-en
-   (reemplazar-en
-    (reemplazar-en
-     (reemplazar-en cubo 1 cara-front-nuevo)
-     2 cara-der-nuevo)
-    3 cara-post-nuevo)
-   4 cara-izq-nuevo))
-
+    [(and (equal? (string-ref (symbol->string mov) 0) #\C)
+          (equal? (string-ref (symbol->string mov) 2) #\B))
+     (rotar-columna-en-cubo n cubo (numero (string-ref (symbol->string mov) 1)) 'abajo)]))
 
